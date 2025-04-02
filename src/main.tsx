72bc9ddbf171af3105139cff0c77115601018ea4
Radim <radimkotajny@gmail.com>
2025-04-02 18:45:59 +0200

    commented undefined font

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
-import '@fontsource-variable/outfit';
+//import '@fontsource-variable/outfit';
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
